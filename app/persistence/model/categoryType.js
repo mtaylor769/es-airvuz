2ea3202a6efb8f25a0581d11e47ba554bf026b7e
Martin Euerle <marty@airvuz.com>
2016-02-29 14:28:41 -0600

    updated data models for: categoryType, and videos

---
+++ app/persistence/model/categoryType.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var categoryTypeSchema = mongoose.Schema({
+
+	/*
+	 * An optional background image for the category.
+	 */
+	backGroundImage : {
+		required : false,
+		type			: String
+	},
+
+	/*
+	 * The model of the drone.
+	 */
+	name: {
+		required	: true,
+		type			: String
+	},
+	
+	/*
+	 * Identifies if the camera name is visible.
+	 */
+	isVisible: {
+		required	: true,
+		type			: Boolean
+	}
+
+});
+
+mongoose.model('CategoryType', categoryTypeSchema);
