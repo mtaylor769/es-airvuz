83d17c005eeec9838f26c1627a4c1d2e55d23fa3
ezekielthao <ezekiel.thao2@gmail.com>
2016-03-02 06:44:35 -0600

    crud operations code

--- app/persistence/crud/users.js
+++ app/persistence/crud/users.js
@@ -133,7 +133,164 @@ User.prototype.create = function(params) {
 				}
 			});
 			
-		});
+		})
 			
 	);
 }
+
+User.prototype.getAllUsers = function(params) {
+	var preCondition = this.getPreCondition({ sourceLocation : "persistence.crud.Users.create"});
+	var validation = preCondition.validate(params);
+	return(new Promise(function(resolve, reject){
+
+		if (validation.data.sessionId === null) {
+			reject(validation.errors);
+		} else {
+			UsersModel.find({}).exec()
+			.then(function(allUsers){
+				var param = {
+					status 	: "200",
+					message : "Okay",
+					users 	: allUsers
+				};
+				resolve(param);
+			})
+			.error(function(e) {
+				var errorMessage		= new ErrorMessage();
+				errorMessage.getErrorMessage({
+					statusCode			: "500",
+					errorMessage 		: "Failed while getting users",
+					sourceError			: e,
+					sourceLocation		: "persistence.crud.Users.getAllAusers"
+				});
+				reject(errorMessage.getErrors());
+			});
+		}
+
+
+
+	}));
+
+	
+}
+
+User.prototype.getUserById = function (params) {
+	var preCondition = this.getPreCondition({ sourceLocation : "persistence.crud.Users.getUserById"});
+	var validation = preCondition.validate(params);
+	return(new Promise(function(resolve, reject){
+		if (validation.data.sessionId === null || validation.data.userId === null) {
+			reject(validation.errors);
+		} else {
+			UsersModel.find({_id : validation.data.userId}).exec()
+			.then(function(user){
+				resolve(user);
+			})
+			.error(function(e){
+				var errorMessage		= new ErrorMessage();
+				errorMessage.getErrorMessage({
+					statusCode			: "500",
+					errorMessage 		: "Failed while getting user by Id",
+					sourceError			: e,
+					sourceLocation		: "persistence.crud.Users.getAllAusers"
+				});
+				reject(errorMessage.getErrors());
+			});
+		}
+
+	}));
+
+	
+}
+
+User.prototype.getUserByEmail = function (params) {
+	var preCondition = this.getPreCondition({ sourceLocation : "persistence.crud.Users.getUserByEmail"});
+	var validation = preCondition.validate(params);
+	return(new Promise(function(resolve, reject){
+		if (validation.data.sessionId === null || validation.data.emailAddress === null) {
+			reject(validation.errors);
+		} else {
+			UsersModel.find({emailAddress : validation.data.emailAddress}).exec()
+			.then(function(user){
+				resolve(user);
+			})
+			.error(function(e){
+				var errorMessage		= new ErrorMessage();
+				errorMessage.getErrorMessage({
+					statusCode			: "500",
+					errorMessage 		: "Failed while getting user by Email",
+					sourceError			: e,
+					sourceLocation		: "persistence.crud.Users.getUserByEmail"
+				});
+				reject(errorMessage.getErrors());
+			});
+		}
+
+	}));
+
+	
+}
+
+User.prototype.getUserByUserName = function (params) {
+	var preCondition = this.getPreCondition({ sourceLocation : "persistence.crud.Users.getUserByUserName"});
+	var validation = preCondition.validate(params);
+	return(new Promise(function(resolve, reject){
+		if (validation.data.sessionId === null || validation.data.emailAddress === null) {
+			reject(validation.errors);
+		} else {
+			UsersModel.find({userName : validation.data.userName}).exec()
+			.then(function(user){
+				resolve(user);
+			})
+			.error(function(e){
+				var errorMessage		= new ErrorMessage();
+				errorMessage.getErrorMessage({
+					statusCode			: "500",
+					errorMessage 		: "Failed while getting user by user name",
+					sourceError			: e,
+					sourceLocation		: "persistence.crud.Users.getUserByUserName"
+				});
+				reject(errorMessage.getErrors());
+			});
+		}
+
+	}));
+}
+
+User.prototype.update = function (params) {
+	var preCondition = this.getPreCondition({ sourceLocation : "persistence.crud.Users.getUserByUserName"});
+	var validation = preCondition.validate(params);
+	return(new Promise(function(resolve, reject){
+		if (validation.errors !=== null) {
+			reject(validation.errors);
+		} else {
+			//Code to update user
+		}
+
+	}));
+}
+
+User.prototype.delete = function(params) {
+	var preCondition = this.getPreCondition({ sourceLocation : "persistence.crud.Users.getUserByUserName"});
+	var validation = preCondition.validate(params);
+	return(new Promise(function(resolve, reject){
+		if (validation.data.sessionId === null || validation.data.userId === null) {
+			reject(validation.errors);
+		} else {
+			UsersModel.delete({_id : validation.data.userId}).exec()
+			.then(function(user){
+				resolve(user);
+			})
+			.error(function(e){
+				var errorMessage		= new ErrorMessage();
+				errorMessage.getErrorMessage({
+					statusCode			: "500",
+					errorMessage 		: "Failed while deleting user",
+					sourceError			: e,
+					sourceLocation		: "persistence.crud.Users.delete"
+				});
+				reject(errorMessage.getErrors());
+			});
+		}
+	}));
+}
+
