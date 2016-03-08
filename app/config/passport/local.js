5f7337f5db6b50fb874c02657f7c92623a3a3344
ezekielthao <ezekiel.thao2@gmail.com>
2016-03-08 15:02:28 -0600

    updating crud operations

--- app/config/passport/local.js
+++ app/config/passport/local.js
@@ -1,30 +1,59 @@
 "use strict";
 
-let _ = require('lodash');
 let LocalStrategy = require('passport-local').Strategy;
 let Users =  require('../../persistence/crud/users');
-let Promise = require('bluebird');
 
-const PROVIDER_KEY = 'local';
+module.exports = function(passport){
+  passport.use('local-signup', new LocalStrategy({
+    firstName           : 'firstName',
+    lastName            : 'lastName',
+    userName            : 'userName',
+    emailAddress        : 'email',
+    password            : 'password'
+  },
+  function(req, email, password, done){
+    process.nextTick(function(){
+      var userPromise = Users.getUserByEmail(email);
+      user.then(function(user) {
+        //console.log(user);
+        if (!user) {
+          var newUser = {
+            firstName         : req.firstName,
+            lastName          : req.lastName,
+            userName          : req.userName,
+            emailAddress      : req.emailAddress
+          }
+          //TODO:  Zeke - look into doing this encryption
+          newUser.password = bcrypt(password);
 
-module.exports = new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, function (email, password, done) {
-  var userPromise = Users.getUserByEmail(email);
-  user
-  .then(function(user) {
-    if (!user) {
+          var createUser = Users.create(newUser);
+          return done(null, createUser);
+        }
+        if (!user.verifyPassword(password)) {
+          return done(null, false);
+        }
+        return (null, user);
+      })
+      .error(function(error){
+        return done(null, false);
+      });
+    });
+  }));
+
+  passport.use('local-login', new LocalStrategy({
+    emailAddress            : 'email',
+    password                : 'password'
+  },
+  function(req, email, password, done){
+    var userPromise = Users.getUserByEmail({emailAddress : email});
+    userPromise.then(function(user){
+      if (!user) {
+        return done(null, false);
+      }
+      if (!user.validPassword)
+    })
+    .error(function(error){
       return done(null, false);
-    }
-    if (!user.verifyPassword(password)) {
-      return done(null, false);
-    }
-    return (null, user);
-  })
-  .error(function(error){
-    return
-  });
-});
-
-module.exports.PROVIDER_KEY = PROVIDER_KEY;
+    });
+  }));
+};
