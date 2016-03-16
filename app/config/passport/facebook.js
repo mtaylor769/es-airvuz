fb21c6034850092645d1bb6b69c6f126f8f4bdda
ezekielthao <ezekiel.thao2@gmail.com>
2016-03-16 15:12:54 -0500

    removing console logs from passport strategy-facebook

--- app/config/passport/facebook.js
+++ app/config/passport/facebook.js
@@ -4,9 +4,6 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var SocialMedia =  require('../../persistence/crud/socialMediaAccount');
 
 module.exports = function(passport, config) {
-  console.log('client ID: ' + config.facebook.clientID);
-  console.log('secret' + config.facebook.clientSecret);
-  console.log('callback url' + config.facebook.callbackURL);
   passport.use(new FacebookStrategy({
     clientID: config.facebook.clientID,
     clientSecret: config.facebook.clientSecret,
