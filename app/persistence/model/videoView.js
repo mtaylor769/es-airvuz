b3f8f30cd2ec8c3689472a6a805079411dd2436c
Martin Euerle <marty@airvuz.com>
2016-03-24 15:18:45 -0500

    adding videoLike, videoView

---
+++ app/persistence/model/videoView.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var videoViewSchema = mongoose.Schema({
+
+	/*
+	 * Date the video was viewed.
+	 */
+	createdDate: {
+		default:	Date.now,
+		type:			Date
+	},
+	
+	/*
+	 * Id of the video that was viewed.
+	 */
+	
+	videoId : {
+		type: mongoose.Schema.ObjectId, ref: 'Video'
+	},
+
+	/*
+	 * The userId of the person who viewed the video.
+	 */
+	userId : {
+		type: mongoose.Schema.ObjectId, ref: 'User'
+	}
+	
+});
+
+mongoose.model('VideoView', videoViewSchema);
