4d10b6785a63321fd5a799a338f80e7c18ffb98a
ezekielthao <ezekiel.thao2@gmail.com>
2016-03-29 14:22:35 -0500

    finished passport strategy for instagram

--- app/config/passport/instagram.js
+++ app/config/passport/instagram.js
@@ -1,7 +1,10 @@
 "use strict";
 
 var InstagramStrategy    = require('passport-instagram').Strategy;
-var UserService       = require('../../utils/service/users.service');
+var UsersCrud            = require('../../persistence/crud/users');
+var SocialCrud           = require('../../persistence/crud/socialMediaAccount');
+var log4js               = require('log4js');
+var logger               = log4js.getLogger('app.config.passport.google');
 
 module.exports = function(passport, config) {
 
@@ -19,8 +22,6 @@ module.exports = function(passport, config) {
     callbackURL: config.instagram.callbackURL,
     passReqToCallback: true
   }, function (req, accessToken, refreshToken, profile, cb) {
-    console.log('profile');
-    console.log(profile);
     var data = {
       provider    : profile.provider,
       accountData : profile,
@@ -32,30 +33,17 @@ module.exports = function(passport, config) {
     socialAccount.then(function(account){
       if (account) {
         logger.debug('found social media account in social media model');
-        logger.info(account);
-        var findUser = UsersCrud.getUserByEmail(data.email);
+        var findUser = UsersCrud.getUserBySocialId(account._id);
         findUser.then(function(user){
           if (user && user._id) {
-            logger.debug('found user - check to see social media ids match');
-            if (user.socialMediaAccounts.indexOf(account._id) < 0) {
-              //add social media id to user
-            }
+            logger.debug('user found with social media id returning user');
             return cb(null, user);
           } else {
-            logger.debug('no user found with email, searching for existing social media id');
-            findUser = UsersCrud.getUserBySocialId(account._id);
-            findUser.then(function(user){
-              if (user && user._id) {
-                logger.debug('user found with social media id returning user');
-                return cb(null, user);
-              } else {
-                logger.debug('no user found, prompt to create new user');
-                //req needs information about user data and social media id
-                data.socialMediaAccounts = account._id;
-                req.newUserInfo = data;
-                return cb(null, null);
-              }
-            });
+            logger.debug('no user found, prompt to create new user');
+            //req needs information about user data and social media id
+            data.socialMediaAccounts = account._id;
+            req.newUserInfo = data;
+            return cb(null, null);
           }
         });
       } else {
@@ -64,26 +52,14 @@ module.exports = function(passport, config) {
         account.then(function(newAccount){
           if (newAccount && newAccount._id) {
             logger.debug('created new social media account in social media model');
-            logger.debug('search user model for existing user with email');
-            var findUser = UsersCrud.getUserByEmail(data.email);
-            findUser.then(function(user){
-              if (user && user._id) {
-                logger.debug('user with email currently exists, adding social media id and returing user');
-                //add social media id to current user and return user
-                return cb(null, user);
-              } else {
-                logger.trace('Needs to be passed back to client to verify and create new account');
-                data.socialMediaAccounts = newAccount._id;
-                req.newUserInfo = data;
-                return cb(null, null);
-              }
-            });
+            logger.trace('Needs to be passed back to client to verify and create new account');
+            data.socialMediaAccounts = newAccount._id;
+            req.newUserInfo = data;
+            return cb(null, null);
+          };
             
-          }
         });
-      }
+      };
     });
-
   }));
-
 };
