506bc0d6861566afcb443ba8a090e4e2b25d1517
Matthew Brown <devPc@Matthews-MacBook-Pro.local>
2016-03-30 16:05:41 -0500

    camera model changed

--- app/persistence/crud/cameraType.js
+++ app/persistence/crud/cameraType.js
@@ -1,27 +1,17 @@
-try {
-  var Promise = require('bluebird');
-  var mongoose = require('mongoose');
-  var log4js = require('log4js');
-  var logger = log4js.getLogger('app.persistence.crud.cameraType');
-  var ErrorMessage = require('../../utils/errorMessage');
-  var ObjectValidationUtil = require('../../utils/objectValidationUtil');
-  var PersistenceException = require('../../utils/exceptions/PersistenceException');
-  var ValidationException = require('../../utils/exceptions/ValidationException');
-  var CameraTypeModel = null;
-  var database = require('../database/database');
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var log4js = require('log4js');
+var logger = log4js.getLogger('app.persistence.crud.cameraType');
+var ErrorMessage = require('../../utils/errorMessage');
+var ObjectValidationUtil = require('../../utils/objectValidationUtil');
+var PersistenceException = require('../../utils/exceptions/PersistenceException');
+var ValidationException = require('../../utils/exceptions/ValidationException');
+var CameraTypeModel = null;
+var database = require('../database/database');
 
-  CameraTypeModel = database.getModelByDotPath({modelDotPath: "app.persistence.model.main.cameraType"});
+  CameraTypeModel = database.getModelByDotPath({modelDotPath: "app.persistence.model.cameraType"});
+logger.debug('loaded camera model');
 
-  if (global.NODE_ENV === "production") {
-    logger.setLevel("INFO");
-  }
-
-  logger.debug("import complete");
-}
-
-catch(exception) {
-  logger.error(" import error:" + exception);
-}
 var CameraType = function(){
 
 };
