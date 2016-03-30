85b9bdb24b4fc9ef869b386c6ad55519b1eb466a
Matthew Brown <devPc@Matthews-MacBook-Pro.local>
2016-03-30 10:18:13 -0500

    comment api routes done

--- app/routes/api/routes.js
+++ app/routes/api/routes.js
@@ -125,4 +125,12 @@ apiRouter.route('/api/comment')
   .post(comment.post)
   .get(comment.get);
 
+apiRouter.route('/api/comment/byParent')
+  .get(comment.getByParentCommentId);
+
+apiRouter.route('/api/comment/:id')
+  .get(comment.getById)
+  .put(comment.put)
+  .delete(comment.delete);
+
 module.exports = apiRouter;
