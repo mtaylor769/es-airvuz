aafaab8958a9648b63325f08dd5bdf569578ecae
Martin Euerle <marty@airvuz.com>
2016-04-03 12:09:03 -0500

    checkpoint

--- app/views/model/indexModel.js
+++ app/views/model/indexModel.js
@@ -34,12 +34,31 @@ IndexModel.prototype.getData = function(params) {
 		logger.info("getData 1.0");
 		logger.info("getData sourceManifest['airvuz.css']:" + sourceManifest["airvuz.css"]);
 		params.data										= {};
-		params.data.airvuz						= {};
 		
 		params.data.index							= {};
 		params.data.index.airvuz			= {};
 		params.data.index.airvuz.css	= sourceManifest["airvuz.css"];
+		
+		
+		params.data.index.head				= {};
+		params.data.index.head.og			= {
+			url : "http://www.airvuz.com"
+		}
 		params.data.index.viewName		= "Index PageV2";		
+		
+		
+	/*
+	        <meta property="og:url" content="http://airvuz.com/"/>
+        <meta property="og:type" content="website" >
+        <meta property="og:title" content="AirVūz – Drone Video Community" >
+        <meta property="og:updated_time" content="1458850844571">
+        <meta property="og:description" content="Discover, watch, and share aerial videos captured by cameras from drones, quadcopters, multi-copters, and radio controlled airplanes and helicopters." >
+        <meta property="og:image" content="http://airvuz.com/assets/img/airvuz_banner.png" >
+        <meta property="fb:app_id" content="441356432709973">
+	 */	
+		
+		
+		
 
 		resolve(params);
 	});  	
