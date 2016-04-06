319867848aaa037819e8db82e94115b900dd9166
Martin Euerle <marty@airvuz.com>
2016-04-06 12:09:44 -0500

    adding initial followers, and notifications schema

---
+++ app/persistence/model/notifications.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+/*
+ * 
+ */
+var NotificationsSchema = mongoose.Schema({
+	
+	/*
+	 * The userId of the person who has taken an action that will be notified to another user.
+	 */
+	actionUserId : {
+		required: true,
+		type: mongoose.Schema.ObjectId, ref: 'User'
+	},	
+	
+	/*
+	 * When the document was created.
+	 */
+	createdDate : {
+		default: Date.now,
+		type: Date
+	},
+	
+	/*
+	 * The notification type:
+	 *	COMMENT | PRIVATE-MESSAGE
+	 */
+	notificationType : {
+		default		: ['COMMENT','PRIVATE-MESSAGE'],
+		required	: true,
+		type			: String
+	},
+	
+	/*
+	 * The userId of the person who is being notified.
+	 */
+	notifiedUserId : {
+		required: true,
+		type: mongoose.Schema.ObjectId, ref: 'User'
+	}
+
+});
+
+module.exports = {
+	connectionName	: "main",
+	modelName				: "NotificationsSchema",
+	schema					: NotificationsSchema
+};
