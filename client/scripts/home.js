a44fd0800b1d7fc0e302431327e49eb7c04f4971
Doua Thao <douathoj@gmail.com>
2016-04-25 09:53:55 -0500

    home page slider with slick

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -1,11 +1,39 @@
 require('../styles/home.css');
 
 function initialize() {
-  /********************************************************/
-  console.group('%cHome :', 'color:red;font:strait');
-  //console.log(Home);
-  console.groupEnd();
-  /********************************************************/
+  $('.video-slick').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          draggable: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: true,
+          arrows: false
+        }
+      }
+    ]
+  });
 }
 
 module.exports = {
