892f17a785c0fff354a693cf0ead5130d92b992f
Matthew Brown <devPc@Matthews-MacBook-Pro.local>
2016-04-27 15:57:20 -0500

    first pass on user profile and video display

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -23,23 +23,8 @@ var user = identity;
 // *************** start JQuery ***********************
 
 $(document).ready(function() {
-//
-	console.log("videoPage: document.ready");
-  var videoId = {};
-  videoId.videoId = window.location.pathname.substring(13);
 
-  $.ajax({
-      type: 'POST',
-      url: '/api/videos/loaded',
-      data: videoId,
-      dataType: 'json'
-  })
-  .done(function(response) {
-    console.log(response);
-  })
-  .error(function(error) {
-    console.log(error);
-  });
+	console.log("videoPage: document.ready");
 
 //
 // *************** start video player page JS ***********************
@@ -314,6 +299,21 @@ if (token) {
   });
 
 //
+  var videoId = {};
+  videoId.videoId = window.location.pathname.substring(13);
+
+  $.ajax({
+      type: 'POST',
+      url: '/api/videos/loaded',
+      data: videoId,
+      dataType: 'json'
+    })
+    .done(function(response) {
+      console.log(response);
+    })
+    .error(function(error) {
+      console.log(error);
+    });
 });
 // ***************  end JQuery ***********************
 //
