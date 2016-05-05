19e957f8503918221999fe853b84a601ba7c61e5
Doua Thao <douathoj@gmail.com>
2016-05-05 13:45:34 -0500

    add view more button

--- client/scripts/category.js
+++ client/scripts/category.js
@@ -1,7 +1,13 @@
 require('../styles/category.css');
 
 function bindEvents() {
-
+  $('#load-more-btn').on('click', function () {
+    /********************************************************/
+    console.group('%cloading more video... :', 'color:red;font:strait');
+    //console.log(loading more video...);
+    console.groupEnd();
+    /********************************************************/
+  });
 }
 
 function initialize() {
