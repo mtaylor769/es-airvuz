862726584887e5d7f894cbcf51d532132e8be1d9
Doua Thao <douathoj@gmail.com>
2016-05-10 22:15:11 -0500

    refactor video collection

--- app/persistence/crud/videoCollection.js
+++ app/persistence/crud/videoCollection.js
@@ -1,33 +1,16 @@
 'use strict';
 try {
-  var log4js       = require('log4js');
-  var logger       = log4js.getLogger('app.persistance.crud.videoCollection');
+  var log4js                = require('log4js');
+  var logger                = log4js.getLogger('app.persistance.crud.videoCollection');
 
-  var database     = require('../database/database');
-  var VideoCollectionModel   = database.getModelByDotPath({modelDotPath: 'app.persistence.model.videoCollection'});
+  var database              = require('../database/database');
+  var VideoCollectionModel  = database.getModelByDotPath({modelDotPath: 'app.persistence.model.videoCollection'});
+
 } catch (exception) {
   logger.error(' import error:' + exception);
 }
 
 function VideoCollection() {}
-
-/**
- * get featured videos if document doesn't exists then create a featured video collection
- */
-function getFeaturedVideos() {
-  return VideoCollectionModel.findOne({name: 'Featured Videos', user: null}).lean().exec()
-    .then(function (videos) {
-      if (videos) {
-        return videos;
-      }
-      var newCollection = new VideoCollectionModel({name: 'Featured Videos'});
-
-      return newCollection.save()
-        .then(function (collection) {
-          return collection;
-        });
-    });
-}
 
 function getVideo(type) {
   return VideoCollectionModel.findOne({name: type, user: null}).lean().exec()
@@ -35,6 +18,7 @@ function getVideo(type) {
       if (videos) {
         return videos;
       }
+
       var newCollection = new VideoCollectionModel({name: type});
 
       return newCollection.save()
@@ -44,18 +28,26 @@ function getVideo(type) {
     });
 }
 
-function updateFeaturedVideos(videos) {
-  return VideoCollectionModel.findOneAndUpdate({name: 'Featured Videos', user: null}, {videos: videos}).exec();
-}
-
 function updateVideos(type, videos) {
   return VideoCollectionModel.findOneAndUpdate({name: type, user: null}, {videos: videos}).exec();
 }
 
 function getStaffPickVideos(count, page) {
-  var limit = count ? count : 10;
-  var skip = (page ? page : 1) * limit;
-  return VideoCollectionModel.findOne({name: 'Staff Pick Videos', user: null}).populate('videos').lean().exec()
+  //var limit = count ? count : 10;
+  //var skip = (page ? page : 1) * limit;
+
+  return getVideoAndPopulate('Staff Pick Videos');
+}
+
+function getFeaturedVideos(count, page) {
+  //var limit = count ? count : 10;
+  //var skip = (page ? page : 1) * limit;
+
+  return getVideoAndPopulate('Featured Videos');
+}
+
+function getVideoAndPopulate(type) {
+  return VideoCollectionModel.findOne({name: type, user: null}).populate('videos').lean().exec()
     .then(function (collection) {
       return VideoCollectionModel.populate(collection, {path: 'videos.userId', model: 'Users'}).then(function (col) {
         if (col.length === 0) {
@@ -67,23 +59,9 @@ function getStaffPickVideos(count, page) {
     });
 }
 
-function getFeaturedVideos(count, page) {
-  var limit = count ? count : 10;
-  var skip = (page ? page : 1) * limit;
-  return VideoCollectionModel.findOne({name: 'Featured Videos', user: null}).populate('videos').lean().exec()
-    .then(function (collection) {
-      return VideoCollectionModel.populate(collection, {path: 'videos.userId', model: 'Users'}).then(function (col) {
-        if (col.length === 0) {
-          return [];
-        }
-        return col.videos;
-      });
-    });
-}
-
-VideoCollection.prototype.getFeaturedVideos    = getFeaturedVideos;
+VideoCollection.prototype.getFeaturedVideos     = getFeaturedVideos;
 VideoCollection.prototype.getStaffPickVideos    = getStaffPickVideos;
-VideoCollection.prototype.getVideo  = getVideo;
-VideoCollection.prototype.updateVideos  = updateVideos;
+VideoCollection.prototype.getVideo              = getVideo;
+VideoCollection.prototype.updateVideos          = updateVideos;
 
 module.exports = new VideoCollection();
