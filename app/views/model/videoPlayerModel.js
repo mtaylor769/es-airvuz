45d8eaf58108f957e4812915e81d58043c5df8c3
Matthew Brown <devPc@Matthews-MacBook-Pro.local>
2016-05-26 20:06:45 -0500

    fixed profile picture issue for comments

--- app/views/model/videoPlayerModel.js
+++ app/views/model/videoPlayerModel.js
@@ -113,6 +113,7 @@ VideoPlayerModel.prototype.getData = function(params) {
 			comments.forEach(function(comment) {
 				comment.commentDisplayDate = moment(comment.commentCreatedDate).fromNow();
 				console.log(comment.commentCreatedDate);
+				comment.userId.isExternalLink = user.profilePicture.indexOf('http') > -1;
 			});
 			dataObject.comments = comments;
 				return categoryCrud.get();
