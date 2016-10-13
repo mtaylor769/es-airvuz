2fe8a0fe977538a65f3cbc11dddb9a3cbbcf24d5
Bryce Blilie <bryce.blilie@airvuz.com>
2016-10-13 16:00:26 -0500

    completed users, starting videos

--- testV2/api/users.js
+++ testV2/api/users.js
@@ -1,166 +1,254 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server  = 'http://localhost';
-var expect = require('chai').expect;
+var chai        = require('chai');
+var chaiHttp    = require('chai-http');
+var host        = 'http://' + (process.env.HOST || 'localhost');
+var server      = host + ":" + (process.env.PORT || 80);
+var expect      = require('chai').expect;
 
-var token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1N2U5NmE3MTlkZGQyYjAyZjQ0ZjMzNjciLCJhY2xSb2xlcyI6WyJ1c2VyLWdlbmVyYWwiXSwiaWF0IjoxNDc2MjE1NTEzLCJleHAiOjE0Nzg4MDc1MTN9.kE3L7h4ojbmpd1YlAC94DvKvTN4evoaYOQPTlplgCV8';
-var userId = '57e96a719ddd2b02f44f3367';
+var token;
+var userId = '56a7473c2defb658467acb6e';
 var userNameDisplay = 'bryceb';
 
 chai.use(chaiHttp);
 
-describe('Users tests no apiVer', function() {
-    /**
-     * Attempt to get data for a known user
-     * prerequisites: userId exists, valid token
-     */
-    describe('Retrieve user info', function() {
-        it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
-            chai.request(server)
-                .get('/api/users/' + userId)
-                .set('Authorization', token)
-                .end(function (err, res) {
-                    var data = res.body;
-                    expect(res).to.have.status(200);
-                    expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                    expect(data).to.have.property('_id');
-                    expect(data).to.have.property('userNameDisplay');
-                    expect(data).to.have.property('emailAddress');
-                    expect(data).to.have.property('userNameUrl');
-                    expect(data).to.have.property('aclRoles');
-                    done();
-                });
+describe('Users API tests', function () {
+    before(function (done) {
+        chai.request(server)
+            .post('/api/auth')
+            .send({emailAddress: 'bryce.blilie@airvuz.com', password: 'bryc3b'})
+            .end(function (err, res) {
+                token = "Bearer " + res.text;
+                done();
+            });
+    });
+    describe('Users tests no apiVer', function() {
+        /**
+         * Attempt to get data for a known user
+         * prerequisites: userId exists, valid token
+         */
+        describe('Retrieve user info', function() {
+            it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+                chai.request(server)
+                    .get('/api/users/' + userId)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(200);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(data).to.have.property('_id');
+                        expect(data).to.have.property('userNameDisplay');
+                        expect(data).to.have.property('emailAddress');
+                        expect(data).to.have.property('userNameUrl');
+                        expect(data).to.have.property('aclRoles');
+                        done();
+                    });
+            });
+        });
+        /**
+         * Attempt to get data for an invalid user NOTE: in the current version this hangs the request
+         * prereq: token valid
+         */
+        // describe('Retrieve user info', function() {
+        //     it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+        //         chai.request(server)
+        //             .get('/api/users/8888888888888888888888')
+        //             .set('Authorization', token)
+        //             .end(function (err, res) {
+        //                 var data = res.body;
+        //                 expect(res).to.have.status(200);
+        //                 done();
+        //             });
+        //     });
+        // });
+        /**
+         * attempt to search for a user by userNameDisplay
+         * prereq: valid userNameDisplay
+         */
+        describe('Retrieve user info from a valid search', function() {
+            it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+                chai.request(server)
+                    .get('/api/users/search/?username=' + userNameDisplay)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(200);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(data).to.have.property('_id');
+                        expect(data).to.have.property('userNameDisplay');
+                        expect(data).to.have.property('emailAddress');
+                        expect(data).to.have.property('userNameUrl');
+                        expect(data).to.have.property('aclRoles');
+                        done();
+                    });
+            });
+        });
+        describe('Search for an unknown user', function() {
+            it('should return null', function(done) {
+                chai.request(server)
+                    .get('/api/users/search/?username=9871234876129876827638746187')
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(200);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(data).to.be.null;
+                        done();
+                    });
+            });
         });
     });
-    /**
-     * Attempt to get data for an invalid user NOTE: in the current version this hangs the request
-     * prereq: token valid
-     */
-    // describe('Retrieve user info', function() {
-    //     it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
-    //         chai.request(server)
-    //             .get('/api/users/8888888888888888888888')
-    //             .set('Authorization', token)
-    //             .end(function (err, res) {
-    //                 var data = res.body;
-    //                 expect(res).to.have.status(200);
-    //                 done();
-    //             });
-    //     });
-    // });
-    /**
-     * attempt to search for a user by userNameDisplay
-     * prereq: valid userNameDisplay
-     */
-    describe('Retrieve user info from a valid search', function() {
-        it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
-            chai.request(server)
-                .get('/api/users/search/?username=' + userNameDisplay)
-                .set('Authorization', token)
-                .end(function (err, res) {
-                    var data = res.body;
-                    expect(res).to.have.status(200);
-                    expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                    expect(data).to.have.property('_id');
-                    expect(data).to.have.property('userNameDisplay');
-                    expect(data).to.have.property('emailAddress');
-                    expect(data).to.have.property('userNameUrl');
-                    expect(data).to.have.property('aclRoles');
-                    done();
-                });
+
+    describe('Users tests apiVer set to default (1.0.0)', function() {
+        /**
+         * Attempt to get data for a known user
+         * prerequisites: userId exists, valid token
+         */
+        var apiVer = 'apiVer=1.0.0';
+
+        describe('Retrieve user info', function() {
+            it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+                chai.request(server)
+                    .get('/api/users/' + userId + '/?' + apiVer)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(200);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(data).to.have.property('_id');
+                        expect(data).to.have.property('userNameDisplay');
+                        expect(data).to.have.property('emailAddress');
+                        expect(data).to.have.property('userNameUrl');
+                        expect(data).to.have.property('aclRoles');
+                        done();
+                    });
+            });
+        });
+        /**
+         * Attempt to get data for an invalid user NOTE: in the current version this hangs the request
+         * prereq: token valid
+         */
+        // describe('Retrieve user info', function() {
+        //     it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+        //         chai.request(server)
+        //             .get('/api/users/8888888888888888888888')
+        //             .set('Authorization', token)
+        //             .end(function (err, res) {
+        //                 var data = res.body;
+        //                 expect(res).to.have.status(200);
+        //                 done();
+        //             });
+        //     });
+        // });
+        /**
+         * attempt to search for a user by userNameDisplay
+         * prereq: valid userNameDisplay
+         */
+        describe('Retrieve user info from a valid search', function() {
+            it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+                chai.request(server)
+                    .get('/api/users/search/?username=' + userNameDisplay  + '/&' + apiVer)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(200);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(data).to.have.property('_id');
+                        expect(data).to.have.property('userNameDisplay');
+                        expect(data).to.have.property('emailAddress');
+                        expect(data).to.have.property('userNameUrl');
+                        expect(data).to.have.property('aclRoles');
+                        done();
+                    });
+            });
+        });
+        describe('Search for an unknown user', function() {
+            it('should return null', function(done) {
+                chai.request(server)
+                    .get('/api/users/search/?username=9871234876129876827638746187' + '/&' + apiVer)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(200);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(data).to.be.null;
+                        done();
+                    });
+            });
         });
     });
-    describe('Search for an unknown user', function() {
-        it('should return null', function(done) {
-            chai.request(server)
-                .get('/api/users/search/?username=9871234876129876827638746187')
-                .set('Authorization', token)
-                .end(function (err, res) {
-                    var data = res.body;
-                    expect(res).to.have.status(200);
-                    expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                    expect(data).to.be.null;
-                    done();
-                });
+
+    describe('Users tests apiVer set to default (2.0.0)', function() {
+        /**
+         * Attempt to get data for a known user
+         * prerequisites: userId exists, valid token
+         */
+        var apiVer = 'apiVer=2.0.0';
+
+        describe('Retrieve user info', function() {
+            it('should return 400 incorrect API Version', function(done) {
+                chai.request(server)
+                    .get('/api/users/' + userId + '/?' + apiVer)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(400);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(res).to.be.json;
+                        expect(data).to.have.property("error");
+                        done();
+                    });
+            });
+        });
+        /**
+         * Attempt to get data for an invalid user NOTE: in the current version this hangs the request
+         * prereq: token valid
+         */
+        // describe('Retrieve user info', function() {
+        //     it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+        //         chai.request(server)
+        //             .get('/api/users/8888888888888888888888')
+        //             .set('Authorization', token)
+        //             .end(function (err, res) {
+        //                 var data = res.body;
+        //                 expect(res).to.have.status(200);
+        //                 done();
+        //             });
+        //     });
+        // });
+        /**
+         * attempt to search for a user by userNameDisplay
+         * prereq: valid userNameDisplay
+         */
+        describe('Retrieve user info from a valid search', function() {
+            it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
+                chai.request(server)
+                    .get('/api/users/search/?username=' + userNameDisplay  + '/&' + apiVer)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(400);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(res).to.be.json;
+                        expect(data).to.have.property("error");
+                        done();
+                    });
+            });
+        });
+        describe('Search for an unknown user', function() {
+            it('should return null', function(done) {
+                chai.request(server)
+                    .get('/api/users/search/?username=9871234876129876827638746187' + '/&' + apiVer)
+                    .set('Authorization', token)
+                    .end(function (err, res) {
+                        var data = res.body;
+                        expect(res).to.have.status(400);
+                        expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
+                        expect(res).to.be.json;
+                        expect(data).to.have.property("error");
+                        done();
+                    });
+            });
         });
     });
+
 });
 
-
-describe('Users tests apiVer set to default (1.0.0)', function() {
-    /**
-     * Attempt to get data for a known user
-     * prerequisites: userId exists, valid token
-     */
-    describe('Retrieve user info', function() {
-        it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
-            chai.request(server)
-                .get('/api/users/' + userId + '/?apiVer=1.0.0')
-                .set('Authorization', token)
-                .end(function (err, res) {
-                    var data = res.body;
-                    expect(res).to.have.status(200);
-                    expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                    expect(data).to.have.property('_id');
-                    expect(data).to.have.property('userNameDisplay');
-                    expect(data).to.have.property('emailAddress');
-                    expect(data).to.have.property('userNameUrl');
-                    expect(data).to.have.property('aclRoles');
-                    done();
-                });
-        });
-    });
-    /**
-     * Attempt to get data for an invalid user NOTE: in the current version this hangs the request
-     * prereq: token valid
-     */
-    // describe('Retrieve user info', function() {
-    //     it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
-    //         chai.request(server)
-    //             .get('/api/users/8888888888888888888888')
-    //             .set('Authorization', token)
-    //             .end(function (err, res) {
-    //                 var data = res.body;
-    //                 expect(res).to.have.status(200);
-    //                 done();
-    //             });
-    //     });
-    // });
-    /**
-     * attempt to search for a user by userNameDisplay
-     * prereq: valid userNameDisplay
-     */
-    describe('Retrieve user info from a valid search', function() {
-        it('should return user _id, userNameDisplay, emailAddress, userNameUrl, aclRoles', function(done) {
-            chai.request(server)
-                .get('/api/users/search/?username=' + userNameDisplay  + '/&apiVer=1.0.0')
-                .set('Authorization', token)
-                .end(function (err, res) {
-                    var data = res.body;
-                    expect(res).to.have.status(200);
-                    expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                    expect(data).to.have.property('_id');
-                    expect(data).to.have.property('userNameDisplay');
-                    expect(data).to.have.property('emailAddress');
-                    expect(data).to.have.property('userNameUrl');
-                    expect(data).to.have.property('aclRoles');
-                    done();
-                });
-        });
-    });
-    describe('Search for an unknown user', function() {
-        it('should return null', function(done) {
-            chai.request(server)
-                .get('/api/users/search/?username=9871234876129876827638746187' + '/&apiVer=1.0.0')
-                .set('Authorization', token)
-                .end(function (err, res) {
-                    var data = res.body;
-                    expect(res).to.have.status(200);
-                    expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
-                    expect(data).to.be.null;
-                    done();
-                });
-        });
-    });
-});
 
