22c05e6051718cb5ae33ed7d3545cd941cb7482d
Bryce Blilie <bryce.blilie@airvuz.com>
2016-10-16 19:44:25 -0500

    updated users tests and added hireme, follow, contact us, update status, request confirmation email resend, added viewmanager require to users1-0-0, updated customCarousel with new crud file references

--- app/persistence/crud/videoCollection1-0-0.js
+++ app/persistence/crud/videoCollection1-0-0.js
@@ -16,7 +16,7 @@ try {
 
 
 } catch (exception) {
-  logger.error(' import error:' + exception);
+    logger.error(' import error:' + exception);
 }
 
 function VideoCollection() {}
@@ -29,110 +29,110 @@ function updateDateWithMoment(videos) {
 }
 
 function getVideo(type) {
-  return VideoCollectionModel.findOne({name: type, user: null}).lean().exec()
-    .then(function (videos) {
-      if (videos) {
-        return videos;
-      }
+    return VideoCollectionModel.findOne({name: type, user: null}).lean().exec()
+        .then(function (videos) {
+            if (videos) {
+                return videos;
+            }
 
-      var newCollection = new VideoCollectionModel({name: type});
+            var newCollection = new VideoCollectionModel({name: type});
 
-      return newCollection.save()
-        .then(function (collection) {
-          return collection;
+            return newCollection.save()
+                .then(function (collection) {
+                    return collection;
+                });
         });
-    });
 }
 
 function updateVideos(type, videos) {
-  return VideoCollectionModel.findOneAndUpdate({name: type, user: null}, {videos: videos}).exec();
+    return VideoCollectionModel.findOneAndUpdate({name: type, user: null}, {videos: videos}).exec();
 }
 
 function getStaffPickVideos() {
-  return getVideoAndPopulate('Staff Pick Videos');
+    return getVideoAndPopulate('Staff Pick Videos');
 }
 
 function getFeaturedVideos() {
-  return getVideoAndPopulate('Featured Videos');
+    return getVideoAndPopulate('Featured Videos');
 }
 
 function getVideoAndPopulate(type) {
-  var videoFields = 'userId title duration thumbnailPath viewCount uploadDate';
-  var userFields = 'userNameUrl userNameDisplay';
+    var videoFields = 'userId title duration thumbnailPath viewCount uploadDate';
+    var userFields = 'userNameUrl userNameDisplay';
 
-  return VideoCollectionModel.findOne({name: type, user: null}).populate('videos', videoFields).lean().exec()
-    .then(function (collection) {
-      if (collection) {
-        return VideoCollectionModel.populate(collection, {path: 'videos.userId', model: 'Users', select: userFields}).then(function (col) {
-          if (!col || col.length === 0) {
-            return [];
-          }
-          // TODO: slice / limit / paging
-          return col.videos.map(function (video) {
-            video.uploadDate = moment(new Date(video.uploadDate)).fromNow();
-            return video;
-          });
+    return VideoCollectionModel.findOne({name: type, user: null}).populate('videos', videoFields).lean().exec()
+        .then(function (collection) {
+            if (collection) {
+                return VideoCollectionModel.populate(collection, {path: 'videos.userId', model: 'Users', select: userFields}).then(function (col) {
+                    if (!col || col.length === 0) {
+                        return [];
+                    }
+                    // TODO: slice / limit / paging
+                    return col.videos.map(function (video) {
+                        video.uploadDate = moment(new Date(video.uploadDate)).fromNow();
+                        return video;
+                    });
+                });
+            } else {
+                return [];
+            }
         });
-      } else {
-        return [];
-      }
-    });
 }
 
 function createVideoCollection(params) {
-  return(new Promise(function(resolve, reject) {
-    return VideoCollectionModel.findOne({user: params.user, name: params.name}).populate('videos').exec()
-      .then(function(videoCollection) {
-        if(!videoCollection) {
-          var videoCollectionModel = new VideoCollectionModel(params);
-          videoCollectionModel.save(function(error, videoCollection) {
-            if(error) {
-              reject(error);
-            } else {
-              resolve(videoCollection);
-            }
-          });
-        } else {
-          return VideoCollectionModel.populate(videoCollection, {path: 'videos.userId', model: 'Users'}).then(function (col) {
-            col.videos.map(function (video) {
-              video.uploadDate = moment(new Date(video.uploadDate)).fromNow();
-              return video;
-            });
+    return(new Promise(function(resolve, reject) {
+            return VideoCollectionModel.findOne({user: params.user, name: params.name}).populate('videos').exec()
+                .then(function(videoCollection) {
+                    if(!videoCollection) {
+                        var videoCollectionModel = new VideoCollectionModel(params);
+                        videoCollectionModel.save(function(error, videoCollection) {
+                            if(error) {
+                                reject(error);
+                            } else {
+                                resolve(videoCollection);
+                            }
+                        });
+                    } else {
+                        return VideoCollectionModel.populate(videoCollection, {path: 'videos.userId', model: 'Users'}).then(function (col) {
+                            col.videos.map(function (video) {
+                                video.uploadDate = moment(new Date(video.uploadDate)).fromNow();
+                                return video;
+                            });
 
-            resolve(videoCollection);
-          });
-        }
-      });
-    })
-  );
+                            resolve(videoCollection);
+                        });
+                    }
+                });
+        })
+    );
 }
 
 
 function getCollectionVideos(userId, name) {
-  return VideoCollectionModel.findOne({user: userId, name: name}).populate('videos').exec();
+    return VideoCollectionModel.findOne({user: userId, name: name}).populate('videos').exec();
 }
 
 function updateCollection(params) {
-  return VideoCollectionModel.findOne({user: params.user, name: params.name}).exec()
-    .then(function(videoCollection) {
-      var videoId = JSON.stringify(params.video);
-      var videoCollectionString = JSON.stringify(videoCollection);
-      var found = videoCollectionString.indexOf(videoId);
-      logger.debug(found);
-      if(found !== -1){
-        return VideoCollectionModel.findOneAndUpdate({user: params.user, name: params.name}, {$pull: {videos: params.video}}, {safe: true}).exec();
-      } else {
-        return VideoCollectionModel.findOneAndUpdate({user: params.user, name: params.name}, {$push: {videos: params.video}}, {safe: true, upsert: true}).exec();
-      }
-    })
+    return VideoCollectionModel.findOne({user: params.user, name: params.name}).exec()
+        .then(function(videoCollection) {
+            var videoId = JSON.stringify(params.video);
+            var videoCollectionString = JSON.stringify(videoCollection);
+            var found = videoCollectionString.indexOf(videoId);
+            logger.debug(found);
+            if(found !== -1){
+                return VideoCollectionModel.findOneAndUpdate({user: params.user, name: params.name}, {$pull: {videos: params.video}}, {safe: true}).exec();
+            } else {
+                return VideoCollectionModel.findOneAndUpdate({user: params.user, name: params.name}, {$push: {videos: params.video}}, {safe: true, upsert: true}).exec();
+            }
+        })
 }
 
 function addToCollectionVideos(userId, name, video) {
-  return VideoCollectionModel.findOneAndUpdate({user: userId, name: name}, {$push: {videos: video}}, {safe: true, upsert: true}).exec();
+    return VideoCollectionModel.findOneAndUpdate({user: userId, name: name}, {$push: {videos: video}}, {safe: true, upsert: true}).exec();
 }
 
 function removeFromCollectionVideos(userId, name, video) {
-  return VideoCollectionModel.findOneAndUpdate({user: userId, name: name}, {$pull: {videos: video}},{safe: true}).exec();
+    return VideoCollectionModel.findOneAndUpdate({user: userId, name: name}, {$pull: {videos: video}},{safe: true}).exec();
 }
 
 function getCurrentCustomCarousel() {
@@ -179,11 +179,11 @@ function findByUrlId(id) {
 }
 
 function findByUserId(id) {
-  return VideoCollectionModel.find({user: id}).exec();
+    return VideoCollectionModel.find({user: id}).exec();
 }
 
 function remove(id) {
-  return VideoCollectionModel.findByIdAndRemove(id).exec();
+    return VideoCollectionModel.findByIdAndRemove(id).exec();
 }
 
 
