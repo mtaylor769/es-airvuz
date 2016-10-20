fd8425f9c08de8221a0e42cf76e0bb3fa7f0645d
Bryce Blilie <bryce.blilie@airvuz.com>
2016-10-20 12:49:17 -0500

    added matts change from oct17 12:23pm to reports1-0-0.js

--- app/routes/apiVersion/reports1-0-0.js
+++ app/routes/apiVersion/reports1-0-0.js
@@ -34,7 +34,7 @@ function getVideos(req, res) {
   var username = req.query.username;
   var startDate = req.query.startDate;
   var endDate = req.query.endDate;
-  
+
   userCrud1_0_0.getByUserName(username)
     .then(function(user) {
       logger.debug(user);
@@ -43,7 +43,7 @@ function getVideos(req, res) {
     .then(function(videos) {
       res.send(videos)
     })
-  
+
 }
 /**
  * route: GET /api/reports/comments
