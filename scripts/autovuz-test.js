0a7c66d7471592e28e8e8bddc9e156da878128d2
Karl Jones <karl.jones@airvuz.com>
2016-11-17 14:48:11 -0600

    Added code to generate array of timestamps

--- scripts/autovuz-test.js
+++ scripts/autovuz-test.js
@@ -1,9 +1,12 @@
-console.log("in");
+console.log("begin");
 
-// requires: sudo rpm install --save probability-distributions
+// requires: sudo npm install --save probability-distributions
 
 try {
     var PD = require("probability-distributions");
+    var log4js = require('log4js');
+    var logger = log4js.getLogger('scripts.autovuz-test.js');
+    var moment = require('moment');
 
     /**
      *
@@ -41,8 +44,39 @@ try {
         }
 
         return(distribution);
+    }
 
 
+    var getTimestamps = function (params) {
+        var theDate             = params.dateBegin;
+        var dist                = params.dist;
+        var distLen             = dist.length;
+        var millisecPerDay      = 86400000;  // number of milliseconds per day
+        var numViews            = 0;
+        var index               = 0;
+        var index2              = 0;
+        var timestamps          = [];        // fill and return this array;
+
+        for(index = 0; index < distLen; index++) {
+            numViews = dist[index];
+
+            if (numViews === 0) {
+                theDate = theDate.add(1, 'days');
+                continue;
+            }
+
+            millisecUnits = parseInt (millisecPerDay / numViews);  // Units of time per day, one view per unit
+
+            for (index2 = 0; index2 < numViews; index2++ ) {
+
+                timestamps.push(theDate  + index2 * millisecUnits );
+            }
+
+            theDate = theDate.add(1, 'days');
+
+        }
+
+        return (timestamps);
     }
 
     // Get a distribution
@@ -59,6 +93,22 @@ try {
     });
 
     // Get a list of timestamps for views.
+
+    var thisDate = moment();        // Time begins now
+
+    var timestamps = getTimestamps ( {
+        dateBegin: thisDate,
+        dist: dist
+    } );
+
+    // TEST:  display values in console
+    var index = 0;
+    var tsLen = timestamps.length;
+    for (index=0; index < tsLen; index++){
+        logger.info( moment (timestamps[index]).toDate() );
+    }
+
+
 
     var noop = "";
 }
