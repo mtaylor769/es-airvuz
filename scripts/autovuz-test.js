531435d2647e5ddef1394ba13a28611100c3b452
Martin Euerle <marty@airvuz.com>
2016-11-17 09:43:13 -0600

    added some comments

--- scripts/autovuz-test.js
+++ scripts/autovuz-test.js
@@ -5,6 +5,19 @@ console.log("in");
 try {
     var PD = require("probability-distributions");
 
+    /**
+     *
+     * Data Model for collection : autoviews:
+     videoId:
+     numberOfViewsToAdd		: number
+     distubutionCurve		: spike, longtail
+     viewAddDateTime		: []  // this is an array date/times when the view is to be added.
+     lastAddedTimeIndex     : index of the last view added  from the date/time distributeion array.
+     lastViewAddedDateTime 	: date
+     isComplete				: boolean
+
+     */
+
     var ToViewsPerDay = function(params) {
         var numberOfDays    = params.numberOfDays;
         var data            = params.data;
@@ -32,18 +45,20 @@ try {
 
     }
 
-
+    // Get a distribution
     var numOfDays = 14;
     var numOfViews = 100;
     var probability = 0.2;  // 0.2 is front loaded, 0.5 is mid, 0.8 is tail loaded
 
     var rbinom_0 = PD.rbinom(numOfViews, numOfDays, 0.2);
 
+    // turn distribution into views per day.
     var dist = ToViewsPerDay({
         numberOfDays    : numOfDays,
         data            : rbinom_0
     });
 
+    // Get a list of timestamps for views.
 
     var noop = "";
 }
