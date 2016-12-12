05a6b61f45f10ddb219fa73fa9ec177ef41ae80c
Doua Thao <doua@airvuz.com>
2016-12-12 12:53:43 -0600

    track xhr request

--- client/scripts/config/jquery.js
+++ client/scripts/config/jquery.js
@@ -1,4 +1,5 @@
 var PubSub        = require('pubsub-js');
+var AVEventTracker = require('../avEventTracker');
 
 $.ajaxSetup({
   beforeSend: function (xhr) {
@@ -14,6 +15,17 @@ $.ajaxSetup({
       if (jqXHR.responseText === 'suspended') {
         PubSub.publish('logout');
       }
+    },
+    500: function (jqXHR) {
+      AVEventTracker({
+        codeSource: 'config:jquery',
+        eventName: 'xhr:500:client',
+        eventType: 'xhr-status',
+        data: {
+          url: this.url,
+          response: jqXHR.responseText
+        }
+      });
     }
   }
 });
