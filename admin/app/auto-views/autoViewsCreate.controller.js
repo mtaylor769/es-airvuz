8540f2fb139a1ac5b08118223d28242b6682a872
Doua Thao <doua@airvuz.com>
2016-12-27 13:33:01 -0600

    remove jscsrc requireLineFeedAtFileEnd and reformat code

--- admin/app/auto-views/autoViewsCreate.controller.js
+++ admin/app/auto-views/autoViewsCreate.controller.js
@@ -1,31 +1,30 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
+    angular
     .module('AirvuzAdmin')
     .controller('autoViewsCreateController', autoViewsCreateController);
 
-  autoViewsCreateController.$inject = ['$http', 'dialog'];
+    autoViewsCreateController.$inject = ['$http', 'dialog'];
 
-  function autoViewsCreateController($http, dialog) {
+    function autoViewsCreateController($http, dialog) {
 
-    function saveAutoView() {
-      $http.post('/api/ava', vm.view).then(function(response) {
+        function saveAutoView() {
+            $http.post('/api/ava', vm.view).then(function (response) {
+                vm.view = {};
+                vm.view.origin = 'admin';
+            }, function (error) {
+                dialog.serverError();
+            });
+        }
+
+        /////////////////////////
+        var vm = this;
         vm.view = {};
         vm.view.origin = 'admin';
-      }, function(error) {
-        dialog.serverError();
-      });
+        vm.probs = ['0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8'];
+
+        vm.saveAutoView = saveAutoView;
     }
-
-  /////////////////////////
-    var vm = this;
-    vm.view = {};
-    vm.view.origin = 'admin';
-    vm.probs = ['0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8'];
-
-    vm.saveAutoView = saveAutoView;
-  }
-
 
 })();
